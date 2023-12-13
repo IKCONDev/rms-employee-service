d6f9aa402b78c51caaabac434511e8a42fbb013b
Mahesh Jarati <mahesh.jarati@ikcontech.com>
2023-12-13 19:14:22 +0530

    Initial Implementation

---
+++ src/main/java/com/ikn/rms/employee/RMSEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.rms.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMSEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RMSEmployeeServiceApplication.class, args);
+	}
+
+}
